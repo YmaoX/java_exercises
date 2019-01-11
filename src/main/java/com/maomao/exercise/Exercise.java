c18ac5c0d89db06279b05f0e4f110c9b2902ba86
grape xu <yuemao.xu@gmail.com>
2019-01-11 14:16:17 +0100

    search in matrix, row first...

--- src/main/java/com/maomao/exercise/Exercise.java
+++ src/main/java/com/maomao/exercise/Exercise.java
@@ -47,7 +47,9 @@ public class Exercise {
 //		System.out.println(uniquePathsDP(7, 3));
 //		System.out.println(uniquePaths2(new int[][] { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 } }));
 //		System.out.println(heightOfTree(new int[] { -1, 0, 1, 6, 6, 0, 0, 2, 7 }));
-		System.out.println(simplyPath("/aaa/../.././bbb//..//c"));
+//		System.out.println(simplyPath("/aaa/../.././bbb//..//c"));
+		final int[][] matrix = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 50 } };
+		System.out.println(searchInMatrix(matrix, 5));
 	}
 
 	/*
@@ -667,6 +669,9 @@ public class Exercise {
 		return maxHeight;
 	}
 
+	/*
+	 * Given an absolute path for a file (Unix-style), simplify it.
+	 */
 	public static String simplyPath(final String path) {
 		if (!path.startsWith("/")) {
 			//must be absolute path
@@ -706,4 +711,54 @@ public class Exercise {
 		return rtn.toString();
 	}
 
+	/*
+	 * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+	 * Integers in each row are sorted from left to right.
+	 * The first integer of each row is greater than the last integer of the previous row.
+	 */
+	public static boolean searchInMatrix(final int[][] matrix, final int target) {
+		//search row
+		int rowStart = 0;
+		int rowEnd = matrix.length - 1;
+		while (rowStart <= rowEnd) {
+			final int mid = (rowStart + rowEnd) / 2;
+			if (matrix[mid][0] == target) {
+				return true;
+			}
+
+			if (matrix[mid][0] < target) {
+				rowStart = mid + 1;
+			} else {
+				rowEnd = mid - 1;
+			}
+		}
+		if (rowEnd < 0) {
+			return false;
+		}
+		logger.debug("row start: {}", rowStart);
+		logger.debug("row end: {}", rowEnd);
+		int row;
+		if (rowStart > matrix.length - 1) {
+			row = matrix.length - 1;
+		} else {
+			row = rowEnd;
+		}
+		logger.debug("row found: {}", row);
+
+		int start = 0;
+		int end = matrix[0].length - 1;
+		while (start <= end) {
+			final int mid = (start + end) / 2;
+			if (matrix[row][mid] == target) {
+				return true;
+			}
+			if (matrix[row][mid] < target) {
+				start = mid + 1;
+			} else {
+				end = mid - 1;
+			}
+		}
+
+		return false;
+	}
 }
