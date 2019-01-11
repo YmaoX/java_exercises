8101c1c0581f7745fdf11a8594f9fe6ca8c5f35c
grape xu <yuemao.xu@gmail.com>
2019-01-11 15:00:19 +0100

    search in matrix v2

--- src/main/java/com/maomao/exercise/Exercise.java
+++ src/main/java/com/maomao/exercise/Exercise.java
@@ -49,7 +49,7 @@ public class Exercise {
 //		System.out.println(heightOfTree(new int[] { -1, 0, 1, 6, 6, 0, 0, 2, 7 }));
 //		System.out.println(simplyPath("/aaa/../.././bbb//..//c"));
 		final int[][] matrix = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 50 } };
-		System.out.println(searchInMatrix(matrix, 5));
+		System.out.println(searchInMatrixV2(matrix, 30));
 	}
 
 	/*
@@ -744,7 +744,6 @@ public class Exercise {
 			row = rowEnd;
 		}
 		logger.debug("row found: {}", row);
-
 		int start = 0;
 		int end = matrix[0].length - 1;
 		while (start <= end) {
@@ -758,7 +757,28 @@ public class Exercise {
 				end = mid - 1;
 			}
 		}
+		return false;
+	}
 
+	public static boolean searchInMatrixV2(final int[][] matrix, final int target) {
+		final int rowNum = matrix.length, colNum = matrix[0].length;
+		int start = 0, end = rowNum * colNum - 1;
+		while (start <= end) {
+			final int mid = (start + end) / 2;
+			final int row = (mid + 1) / colNum;
+			if (row >= rowNum) {
+				return false;
+			}
+			final int col = (mid + 1) % colNum;
+			if (matrix[row][col] == target) {
+				return true;
+			}
+			if (matrix[row][col] < target) {
+				start = mid + 1;
+			} else {
+				end = mid - 1;
+			}
+		}
 		return false;
 	}
 }
